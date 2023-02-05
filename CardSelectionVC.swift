b7eb8c5e79f4ee2627625882f53924be0d1cdaee
SabiDhiman <sabi98@live.co.uk>
2023-02-05 14:58:48 +0000

    added startTimer function to change image

--- CardSelectionVC.swift
+++ CardSelectionVC.swift
@@ -10,21 +10,33 @@ import UIKit
 class CardSelectionVC: UIViewController {
 
     @IBOutlet var cardImageView: UIImageView!
-
-    
-     
     @IBOutlet var buttons: [UIButton]!
     //created an array with all three uibuttons  - must click the dot and drag  to all the remaining buttons
     
+    var timer: Timer!
+    
+    //ALL VARIABLES GO ABOVE VIEWDIDLOAD
+    //CALL FUNCTIONS INSIDE VIEWDIDLOAD
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        startTimer()
+        
 //        stopButton.layer.cornerRadius       = 8
 //        restartButton.layer.cornerRadius    = 8
 //        rulesButton.layer.cornerRadius      = 8
         for button in buttons{
             button.layer.cornerRadius = 8
         }
+    
+    }
+    func startTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    // target refers to class itself
+    //repeat is a bool as you want the timer to repeat
+    //write function to pass into startTimer function as objc method
+    @objc func showRandomImage(){
+        print("timer on")
     }
     
 //before the foor-loop you must create an outlet collection with all the buttons in the array
