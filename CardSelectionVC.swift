9b902b5f1952406ed3a5a2c256f3eee863e8b4ab
SabiDhiman <sabi98@live.co.uk>
2023-02-05 13:54:44 +0000

    cornered all the buttons by creating an outlet collection

--- CardSelectionVC.swift
+++ CardSelectionVC.swift
@@ -13,18 +13,22 @@ class CardSelectionVC: UIViewController {
     
     
     @IBOutlet var stopButton: UIButton!
-    
     @IBOutlet var restartButton: UIButton!
-    
-    
     @IBOutlet var rulesButton: UIButton!
+    
+     
+    @IBOutlet var buttons: [UIButton]!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        stopButton.layer.cornerRadius       = 8
-        restartButton.layer.cornerRadius    = 8
-        rulesButton.layer.cornerRadius      = 8
+//        stopButton.layer.cornerRadius       = 8
+//        restartButton.layer.cornerRadius    = 8
+//        rulesButton.layer.cornerRadius      = 8
+        for button in buttons{
+            button.layer.cornerRadius = 8
+        }
     }
     
 
